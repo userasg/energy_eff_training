2f1d4069aa9051ab8f3fb74c95a2956192efaa8c
Gahir <psyag13@carey.cs.nott.ac.uk>
2025-08-02 15:33:53 +0100

    added sbatch

---
+++ training_models/experiment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -c4 --mem=8g
+#SBATCH --gres gpu:4
+#SBATCH -p cs -q csug
+
+source /usr2/share/gpu.sbatch
+
+python .\main.py --model mobilenet_v2 --mode train_with_random --epoch 10 --save_path cifar10_results/mobilenet_v2 --dataset cifar10 --batch_size 32 --start_revision 9 --task classification --threshold 0.3
